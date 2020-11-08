9e83415f19000fa6b16826f5e1c804cb76436ba2
tchawou-daniel <73182303+tchawou-daniel@users.noreply.github.com>
2020-11-08 19:58:15 +0100

    Create project

---
+++ src/main/java/com/sdjt/tpprogcomposant/TpprogcomposantApplication.java
@@ -0,0 +1,13 @@
+package com.sdjt.tpprogcomposant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpprogcomposantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpprogcomposantApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sdjt/tpprogcomposant/TpprogcomposantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sdjt.tpprogcomposant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpprogcomposantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
